377146f08157f345ef3a102a22f624100739150a
Marat Kalimullin <mariknv86@gmail.com>
2021-03-20 18:43:00 +0500

    added chat, fixed port variable

---
+++ src/main/java/ru/mariknv86/medhelper/controller/ChatController.java
@@ -0,0 +1,39 @@
+package ru.mariknv86.medhelper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.mariknv86.medhelper.dto.request.MessageDTO;
+import ru.mariknv86.medhelper.dto.response.ResultTrueDto;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api/chat")
+@RequiredArgsConstructor
+public class ChatController {
+
+    List<String> chatLog = new ArrayList<>();
+
+    @GetMapping
+    public ResponseEntity<?> getChatUpdates() {
+        return ResponseEntity.ok(chatLog);
+    }
+
+    @PostMapping("/consultant")
+    public ResponseEntity<?> sendMessageFromConsultant(@RequestBody MessageDTO messageDTO) {
+        chatLog.add("Консультант: " + messageDTO.getMessage());
+        return ResponseEntity.ok(new ResultTrueDto());
+    }
+
+    @PostMapping("/client")
+    public ResponseEntity<?> sendMessageFromUser(@RequestBody MessageDTO messageDTO) {
+        chatLog.add("Клиент: " + messageDTO.getMessage());
+        return ResponseEntity.ok(new ResultTrueDto());
+    }
+
+
+}
